686ee990cbfee087f0177eba12ae73e3de4ce37e
PD66188 <PD66188@192.168.56.1>
2024-03-11 17:06:19 +0530

    First Test commit

---
+++ WorkforntTest/src/main/java/com/workfront/WorkforntTestApplication.java
@@ -0,0 +1,13 @@
+package com.workfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforntTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforntTestApplication.class, args);
+	}
+
+}
---
+++ WorkforntTest/src/test/java/com/workfront/WorkforntTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforntTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
