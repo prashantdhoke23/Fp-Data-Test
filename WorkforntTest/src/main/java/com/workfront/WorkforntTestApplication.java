53fafac461c20dd50f6ecbab11d7fd7d6825e327
PD66188 <PD66188@192.168.56.1>
2024-03-11 17:09:05 +0530

    Commit

--- WorkforntTest/src/main/java/com/workfront/WorkforntTestApplication.java
+++ WorkforntTest/src/main/java/com/workfront/WorkforntTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkforntTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkforntTestApplication.class, args);
+		SpringApplication.run(WorkforntTestApplication.class, args); 
 	}
 
 }
