93c535903bcb4206dd1f658592b8395bab9f2b3e
Prashant Dhoke <prashant.dhoke@zensar.com>
2024-03-12 16:04:53 +0530

    New

--- src/main/java/com/workfront/WorkfrontTestApplication.java
+++ src/main/java/com/workfront/WorkfrontTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkforntTestApplication {
+public class WorkfrontTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkforntTestApplication.class, args); 
+		SpringApplication.run(WorkfrontTestApplication.class, args); 
 	}
 
 }
